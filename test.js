7759eb9f12804088f3f3f754301057abbb9ff30f
Ricky Zhuang <ricky.zhuang@oracle.com>
2017-11-28 14:27:46 +0800

    fetching stream test

--- test.js
+++ test.js
@@ -8,16 +8,34 @@ if (process.argv.length < 3) {
 var file = process.argv[2];
 console.log(process.argv);
 var inStream = fs.createReadStream(file);
+var outStream = fs.createWriteStream('output.flv');
+
+var bfHeader;
+var bfFirstTag;
+var nTagIndex = 0;
 
 var onGetHeader = (bfData) => {
    console.log(bfData.length);
+   bfHeader = bfData;
+   outStream.write(bfHeader);
 };
 var onGetTag = (bfData) => {
    console.log(bfData.length);
+   if (bfFirstTag === undefined) {
+      bfFirstTag = bfData;
+      outStream.write(bfFirstTag);
+   }
+   nTagIndex++;
+
+   if (nTagIndex > 1000) {
+      outStream.write(bfData);
+   }
 };
 
 var onGetLastTagSize = (bfData) => {
    console.log(bfData.length);
+   outStream.write(bfData);
+   outStream.end();
 };
 
 flvUtils.parseStream(inStream, {
